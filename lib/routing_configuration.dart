7e9775c30b646ba9a0ca4e3145f8b64426bc2629
Roc Boronat <roc@fewlaps.com>
2023-10-26 10:54:40 +0200

    Place a redirection in a intermediate screen of the onboarding flow

--- lib/routing_configuration.dart
+++ lib/routing_configuration.dart
@@ -1,12 +1,18 @@
+import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:gorouter_playground/example_screen.dart';
 
 // GoRouter configuration
 final goRouter = GoRouter(
+  debugLogDiagnostics: true,
   routes: [
     GoRoute(
       path: '/',
       builder: (context, state) => const ExampleScreen(label: "Welcome screen"),
+    ),
+    GoRoute(
+      path: '/sign-in-needed',
+      builder: (context, state) => const ExampleScreen(label: "Sign-in-needed"),
     ),
     GoRoute(
       path: '/1',
@@ -15,6 +21,13 @@ final goRouter = GoRouter(
         GoRoute(
           path: '2',
           builder: (context, state) => const ExampleScreen(label: "2"),
+          redirect: (BuildContext context, GoRouterState state) {
+            if (true) {
+              return '/sign-in-needed';
+            } else {
+              return null;
+            }
+          },
           routes: [
             GoRoute(
               path: '3',
