c067e9577a7eacc654b3dac47890a72c79f98cd9
Roc Boronat <roc@fewlaps.com>
2023-10-26 10:28:37 +0200

    Create the Flutter project with an example screen

---
+++ android/app/src/main/kotlin/com/fewlaps/gorouter/gorouter_playground/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fewlaps.gorouter.gorouter_playground
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ExampleScreen extends StatelessWidget {
+  final String label;
+
+  const ExampleScreen({super.key, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Page $label"),
+      ),
+      body: Center(
+          child: Text(
+        "This is page $label",
+        style: Theme.of(context).textTheme.headlineLarge,
+      )),
+    );
+  }
+}
